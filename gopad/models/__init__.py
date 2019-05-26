7d2ce274de8865895dc10fd2f1429e2b636ad32a
Thomas Boerger <thomas@webhippie.de>
2019-05-26 21:54:37 +0200

    Integrate first openapi generated client

--- gopad/models/__init__.py
+++ gopad/models/__init__.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 # flake8: noqa
-
 """
     Gopad OpenAPI
 
@@ -14,23 +13,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.0.0-alpha1"
-
-# import apis into sdk package
-from gopad.api.auth_api import AuthApi
-from gopad.api.profile_api import ProfileApi
-from gopad.api.team_api import TeamApi
-from gopad.api.user_api import UserApi
-
-# import ApiClient
-from gopad.api_client import ApiClient
-from gopad.configuration import Configuration
-from gopad.exceptions import OpenApiException
-from gopad.exceptions import ApiTypeError
-from gopad.exceptions import ApiValueError
-from gopad.exceptions import ApiKeyError
-from gopad.exceptions import ApiException
-# import models into sdk package
+# import models into model package
 from gopad.models.auth_token import AuthToken
 from gopad.models.auth_verify import AuthVerify
 from gopad.models.inline_object import InlineObject
@@ -40,4 +23,3 @@ from gopad.models.team_user import TeamUser
 from gopad.models.team_user_params import TeamUserParams
 from gopad.models.user import User
 from gopad.models.user_team_params import UserTeamParams
-
---
+++ hack/generate-client.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ -L $0 ]
+then
+    ROOT=$(realpath -e $(dirname $(readlink -e $0))/..)
+else
+    ROOT=$(realpath -e $(dirname $0)/..)
+fi
+
+SPEC=${SPEC:-https://dl.gopad.tech/openapi/1.0.0-alpha1.yml}
+
+docker run --rm \
+	-v ${ROOT}:/generate \
+	openapitools/openapi-generator-cli:v4.0.0 \
+	generate \
+	-c /generate/openapi.yml \
+	-g python \
+	--git-repo-id gopad-python \
+	--git-user-id gopad \
+	--http-user-agent gopad-python/1.0.0-alpha1 \
+	-i ${SPEC} \
+	-o /generate \
+	-t /generate/hack/openapi/templates \
+	--strict-spec true
