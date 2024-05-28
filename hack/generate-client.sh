78d045b3c5ef4228a698842584d33be6b7dd6b4c
Thomas Boerger <thomas@webhippie.de>
2024-05-28 16:14:30 +0200

    ci: improve release process and separate workflow for openapi

--- hack/generate-client.sh
+++ hack/generate-client.sh
@@ -11,10 +11,11 @@ fi
 SPEC_VERSION="1.0.0-alpha1"
 SPEC_DOWNLOAD=${SPEC:-https://dl.gopad.eu/openapi/${SPEC_VERSION}.yml}
 
-echo "> deleteing library folder"
-rm -rf gopad
 
-if hash openapi-generator-cli 2>/dev/null; then
+pushd "${ROOT}" >/dev/null
+    echo "> deleting library folder"
+    rm -rf gopad
+
     openapi-generator-cli \
         generate \
         -c openapi.yml \
@@ -26,28 +27,4 @@ if hash openapi-generator-cli 2>/dev/null; then
         -o . \
         -t hack/openapi/templates \
         --strict-spec true
-else
-    if hash docker 2>/dev/null; then
-        COMMAND="docker"
-    elif hash podman 2>/dev/null; then
-        COMMAND="postman"
-    else
-        echo "missing container runtime"
-        exit 1
-    fi
-
-    "${COMMAND}" run --rm \
-        -v "${ROOT}:/generate:Z" \
-        -w /generate \
-        docker.io/openapitools/openapi-generator-cli:v7.6.0 \
-        generate \
-        -c /generate/openapi.yml \
-        -g python \
-        --git-repo-id gopad-python \
-        --git-user-id gopad \
-        --http-user-agent "gopad-python/${SPEC_VERSION}" \
-        -i "${SPEC_DOWNLOAD}" \
-        -o /generate \
-        -t /generate/hack/openapi/templates \
-        --strict-spec true
-fi
+popd >/dev/null
